e8605f56b1735e586773b44a5c4d41a235cbaa49
G'k <39066502+Guekka@users.noreply.github.com>
2019-11-29 19:21:10 +0100

    [Meshes] Fixed filename not being set when loading a file

--- source/File/Meshes/MeshFile.cpp
+++ source/File/Meshes/MeshFile.cpp
@@ -16,6 +16,7 @@ int MeshFile::loadFromDisk(const QString &filePath)
 {
     auto meshFile = static_cast<MeshResource *>(&*_file);
     _optimizedCurrentFile = false;
+    _filename = filePath;
     return meshFile->Load(filePath.toStdString());
 }
 
