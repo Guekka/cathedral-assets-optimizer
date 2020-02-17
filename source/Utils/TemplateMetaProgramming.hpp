3386a90bea868b4b0cbdd8567d2059c181b54506
G'k <39066502+Guekka@users.noreply.github.com>
2020-02-17 23:57:03 +0100

    [General] Added TMP utils

---
+++ source/Utils/TemplateMetaProgramming.hpp
@@ -0,0 +1,15 @@
+/* Copyright (C) 2020 G'k
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+#pragma once
+
+#include "pch.hpp"
+
+namespace CAO {
+template<typename T>
+using remove_cv_ref = std::remove_cv_t<std::remove_reference_t<T>>;
+
+template<typename T, typename U>
+using is_equiv = std::is_same<remove_cv_ref<T>, remove_cv_ref<U>>;
+} // namespace CAO
