e1dfc3ee32f0279d728b29fd74fee9ed6ba48afc
G'k <39066502+Guekka@users.noreply.github.com>
2020-05-24 21:34:14 +0200

    [BSA] Using bs_archive included in BSAFile instead of creating a new one

--- src/Commands/BSA/BSAExtract.cpp
+++ src/Commands/BSA/BSAExtract.cpp
@@ -9,7 +9,6 @@
 namespace CAO {
 CommandResult BSAExtract::process(File& file)
 {
-    //TODO We are creating a new BSArchiveAuto while the file is already one
     auto bsafile = dynamic_cast<BSAFileResource *>(&file.getFile(true));
     if (!bsafile)
         return _resultFactory.getCannotCastFileResult();
@@ -21,18 +20,20 @@ CommandResult BSAExtract::process(File& file)
     libbsarch::convertible_string rootPath = QFileInfo(bsaPath).path();
 
     try
-    {
-        libbsarch::bs_archive_auto archive;
-        archive.set_dds_callback(&BSACallback, rootPath);
-        archive.load_from_disk(bsaPath);
-        archive.extract_all(rootPath, false);
+    {;
+        bsafile->set_dds_callback(&BSACallback, rootPath);
+        bsafile->load_from_disk(bsaPath);
+        bsafile->extract_all(rootPath, false);
     }
     catch (const std::exception &e)
     {
         const QString &error = QString("Failed to extract BSA with error message: '%1'").arg(e.what());
         return _resultFactory.getFailedResult(1, error);
     }
-    file.setFile(*new BSAFileResource); //We won't be able to delete the file otherwise
+    QString path = file.getName();
+
+    file.setFile(*new BSAFileResource); //Closing the archive. We won't be able to delete the file otherwise
+
     if (!QFile::remove(file.getName())) //We want to remove the original file, not the backup file
         return _resultFactory.getFailedResult(2, "BSA Extract succeeded but failed to delete the extracted BSA");
 
