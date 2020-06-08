4fabf9a1c1e99438bdf43f26c7e5a7c192864fa4
G'k <39066502+Guekka@users.noreply.github.com>
2020-06-08 14:50:37 +0200

    [General] Improved use of forward references and ADL in Algorithms.hpp

--- src/Utils/Algorithms.hpp
+++ src/Utils/Algorithms.hpp
@@ -10,7 +10,7 @@
 
 //Expects a range sorted in descending order
 template<class It, class Predicate, class Sum>
-[[nodiscard]] static It merge_if(It first, It last, Predicate predicate, Sum sum)
+[[nodiscard]] static It merge_if(It first, It last, Predicate &&predicate, Sum &&sum)
 {
     if (first == last)
         return last;
@@ -18,9 +18,9 @@ template<class It, class Predicate, class Sum>
     last--;
     while (first != last)
     {
-        while (predicate(*first, *last))
+        while (std::forward<Predicate>(predicate)(*first, *last))
         {
-            *first = sum(*first, *last);
+            *first = std::forward<Sum>(sum)(*first, *last);
             last--;
             if (first == last)
                 return ++first;
@@ -31,49 +31,51 @@ template<class It, class Predicate, class Sum>
 }
 
 template<class It, class Predicate>
-[[nodiscard]] static It merge_if(It first, It last, Predicate predicate)
+[[nodiscard]] static It merge_if(It first, It last, Predicate &&predicate)
 {
     using Type = std::remove_cv_t<std::remove_reference_t<decltype(*first)>>;
     auto plus  = [](const Type &first, const Type &second) { return first + second; };
-    return merge_if(first, last, predicate, plus);
+    return merge_if(first, last, std::forward<Predicate>(predicate), plus);
 }
 
 template<class Container, class Predicate>
-[[nodiscard]] static auto merge_if(Container &&cont, Predicate predicate)
+[[nodiscard]] static auto merge_if(Container &cont, Predicate &&predicate)
 {
-    return merge_if(cont.begin(), cont.end(), predicate);
+    using namespace std;
+    return merge_if(begin(cont), end(cont), std::forward<Predicate>(predicate));
 }
 
 template<class Container, class Predicate, class Sum>
-[[nodiscard]] static auto merge_if(Container &&cont, Predicate pred, Sum sum)
+[[nodiscard]] static auto merge_if(Container &cont, Predicate &&pred, Sum &&sum)
 {
-    return merge_if(cont.begin(), cont.end(), pred, sum);
+    using namespace std;
+    return merge_if(begin(cont), end(cont), std::forward<Predicate>(pred), std::forward<Sum>(sum));
 }
 
 template<class It, class ValueType>
-[[nodiscard]] static auto contains(It begin, It end, ValueType value)
+[[nodiscard]] static auto contains(It begin, It end, const ValueType &value)
 {
     return std::find(begin, end, value) != end;
 }
 
 template<class Container, class ValueType>
-[[nodiscard]] static auto contains(Container &&cont, ValueType value)
+[[nodiscard]] static auto contains(const Container &cont, const ValueType &value)
 {
-    return std::find(std::forward<Container>(cont).begin(), std::forward<Container>(cont).end(), value)
-           != std::forward<Container>(cont).end();
+    using namespace std;
+    return contains(begin(cont), end(cont), value);
 }
 
 template<class Container, class Pred>
-[[nodiscard]] static auto any_of(Container &&cont, Pred &&pred)
+[[nodiscard]] static auto any_of(const Container &cont, Pred &&pred)
 {
-    return std::any_of(std::forward<Container>(cont).begin(),
-                       std::forward<Container>(cont).end(),
-                       std::forward<Pred>(pred));
+    using namespace std;
+    return std::any_of(begin(cont), end(cont), std::forward<Pred>(pred));
 }
 
 template<class Container>
 [[nodiscard]] static auto remove_duplicates(Container &cont)
 {
-    std::sort(std::begin(cont), std::end(cont));
-    cont.erase(std::unique(std::begin(cont), std::end(cont)), std::end(cont));
+    using namespace std;
+    std::sort(begin(cont), end(cont));
+    cont.erase(std::unique(begin(cont), end(cont)), end(cont));
 }
