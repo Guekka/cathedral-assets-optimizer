5d23b6e0427eb653ae72f6029f59dcec335a60e8
G'k <39066502+Guekka@users.noreply.github.com>
2020-07-30 13:26:28 +0200

    [UI] Added MeshesAdvancedWindow

---
+++ src/GUI/MeshesAdvancedWindow.cpp
@@ -0,0 +1,65 @@
+/* Copyright (C) 2020 G'k
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "MeshesAdvancedWindow.hpp"
+#include "Settings/GeneralSettings.hpp"
+#include "Settings/PatternSettings.hpp"
+#include "Utils.hpp"
+
+namespace CAO {
+MeshesAdvancedWindow::MeshesAdvancedWindow(QWidget *parent)
+    : IWindowModule(parent)
+    , ui_(new Ui::MeshesAdvancedWindow)
+{
+    ui_->setupUi(this);
+}
+
+void MeshesAdvancedWindow::connectAll(PatternSettings &pSets, GeneralSettings &gSets)
+{
+    connectGroupBox(ui_->mainGroupBox,
+                    ui_->necessaryOptimizationRadioButton,
+                    ui_->mediumOptimizationRadioButton,
+                    ui_->fullOptimizationRadioButton);
+
+    ui_->mainGroupBox->setChecked(pSets.iMeshesOptimizationLevel() == 0);
+
+    connect(ui_->mainGroupBox, &QGroupBox::toggled, &pSets.iMeshesOptimizationLevel, [&pSets](bool state) {
+        if (state)
+            pSets.iMeshesOptimizationLevel = 0;
+    });
+
+    ui_->necessaryOptimizationRadioButton->setChecked(pSets.iMeshesOptimizationLevel() == 1);
+
+    connect(ui_->necessaryOptimizationRadioButton,
+            &QRadioButton::toggled,
+            &pSets.iMeshesOptimizationLevel,
+            [&pSets](bool state) {
+                if (state)
+                    pSets.iMeshesOptimizationLevel = 1;
+            });
+
+    ui_->mediumOptimizationRadioButton->setChecked(pSets.iMeshesOptimizationLevel() == 2);
+
+    connect(ui_->mediumOptimizationRadioButton,
+            &QRadioButton::toggled,
+            &pSets.iMeshesOptimizationLevel,
+            [&pSets](bool state) {
+                if (state)
+                    pSets.iMeshesOptimizationLevel = 2;
+            });
+
+    ui_->fullOptimizationRadioButton->setChecked(pSets.iMeshesOptimizationLevel() == 2);
+
+    connect(ui_->fullOptimizationRadioButton,
+            &QRadioButton::toggled,
+            &pSets.iMeshesOptimizationLevel,
+            [&pSets](bool state) {
+                if (state)
+                    pSets.iMeshesOptimizationLevel = 3;
+            });
+
+    connectWrapper(*ui_->ignoreHeadpartsCheckBox, pSets.bMeshesIgnoreHeadparts);
+}
+} // namespace CAO
---
+++ src/GUI/MeshesAdvancedWindow.hpp
@@ -0,0 +1,26 @@
+/* Copyright (C) 2020 G'k
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#pragma once
+
+#include "IWindowModule.hpp"
+#include "ui_MeshesAdvancedWindow.h"
+
+namespace Ui {
+class MeshesAdvancedWindow;
+}
+
+namespace CAO {
+class MeshesAdvancedWindow : public IWindowModule
+{
+public:
+    explicit MeshesAdvancedWindow(QWidget *parent = nullptr);
+
+    void connectAll(PatternSettings &pSets, GeneralSettings &gSets) override;
+
+private:
+    Ui::MeshesAdvancedWindow *ui_;
+};
+} // namespace CAO
