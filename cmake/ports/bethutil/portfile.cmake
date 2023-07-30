a07851c9e1e6484886edbfdba4c36ce3fad7caff
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-07-30 12:02:56 +0200

    feat: rename bad archive

--- cmake/ports/bethutil/portfile.cmake
+++ cmake/ports/bethutil/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO Guekka/bethutil
-        REF 88d91270176141ff2eef5508ebce76892118069f
-        SHA512 9d8fb8773ec07f1f6c349b6bcc5a9a61264a16dec597183b9ec716d85a698143527e27d55df776c00a87c61ee0687c21470f0548ec4a9dd0be7b065cbfcf42df
+        REF 61822b39367e84e37fbccb9d52b06b73f3f84099
+        SHA512 40a7c0f8bd5eb60b009169060445ae4b9db2a4c0c793b5cad984935280c67f939a1e9265bead1e6c630c438d8e2fe158394e85e7d6ade23966c4c47323443c25
         HEAD_REF main
 )
 
@@ -21,6 +21,6 @@ vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/bethutil")
 file(REMOVE_RECURSE
         ${CURRENT_PACKAGES_DIR}/debug/include
         ${CURRENT_PACKAGES_DIR}/debug/share
-        )
+)
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
--- src/manager.cpp
+++ src/manager.cpp
@@ -25,6 +25,30 @@ Manager::Manager(Settings settings)
 {
 }
 
+constexpr auto k_bad_file_ext = ".caobad";
+
+void rename_bad_file(const std::filesystem::path &file_path)
+{
+    // add k_bad_file_ext to the file name
+    auto new_file_path = file_path;
+    new_file_path += k_bad_file_ext;
+
+    try
+    {
+        PLOG_ERROR << fmt::format("File {} is bad. Renaming to {}",
+                                  file_path.string(),
+                                  new_file_path.string());
+        btu::fs::rename(file_path, new_file_path);
+    }
+    catch (const std::exception &e)
+    {
+        PLOG_ERROR << fmt::format("Failed to rename file {} to {}: {}",
+                                  file_path.string(),
+                                  new_file_path.string(),
+                                  e.what());
+    }
+}
+
 // TODO: use std::chrono (GCC 13.1) instead of QDateTime
 void Manager::run_optimization()
 {
@@ -33,8 +57,8 @@ void Manager::run_optimization()
     const auto start_time = QDateTime::currentDateTime();
     PLOG_INFO << fmt::format("Beginning. Start time: {}", start_time.toString("hh'h'mm'm'").toStdString());
 
-    auto archive_ext = btu::bsa::Settings::get(settings_.current_profile().target_game).extension;
-    auto mod         = btu::modmanager::ModFolder(settings_.current_profile().input_path, archive_ext);
+    auto bsa_sets = btu::bsa::Settings::get(settings_.current_profile().target_game);
+    auto mod      = btu::modmanager::ModFolder(settings_.current_profile().input_path, bsa_sets);
 
     PLOG_INFO << "Counting files...";
 
@@ -42,48 +66,72 @@ void Manager::run_optimization()
 
     emit files_counted(mod.size());
 
-    mod.transform([this](btu::modmanager::ModFolder::ModFile &&file) -> std::optional<std::vector<std::byte>> {
-        auto path         = file.relative_path;
-        auto path_for_log = btu::common::as_ascii_string(path.u8string());
+    mod.transform(
+        [this](btu::modmanager::ModFolder::ModFile &&file) -> std::optional<std::vector<std::byte>> {
+            auto path         = file.relative_path;
+            auto path_for_log = btu::common::as_ascii_string(path.u8string());
 
-        auto dbg_file_orig_size    = file.content.size();
-        auto dbg_file_orig_content = file.content;
-        PLOG_INFO << fmt::format("Processing file {}", path_for_log);
+            auto dbg_file_orig_size    = file.content.size();
+            auto dbg_file_orig_content = file.content;
+            PLOG_INFO << fmt::format("Processing file {}", path_for_log);
 
-        auto ret = process_file(std::move(file), settings_);
+            auto ret = process_file(std::move(file), settings_);
 
-        emit file_processed(path);
+            emit file_processed(path);
 
-        if (!ret)
-        {
-            if (ret.error() == k_error_no_work_required) // everything is fine
+            if (!ret)
+            {
+                if (ret.error() == k_error_no_work_required) // everything is fine
+                    return std::nullopt;
+
+                PLOG_ERROR << fmt::format("Failed to process file {}: {}",
+                                          path_for_log,
+                                          ret.error().message());
+
+                // TODO: rename bad files
+
                 return std::nullopt;
+            }
 
-            PLOG_ERROR << fmt::format("Failed to process file {}: {}", path_for_log, ret.error().message());
-            return std::nullopt;
-        }
+            auto dbg_file_new_size = ret->size();
+            auto dbg_file_ratio    = static_cast<double>(dbg_file_new_size)
+                                  / static_cast<double>(dbg_file_orig_size);
 
-        auto dbg_file_new_size = ret->size();
-        auto dbg_file_ratio    = static_cast<double>(dbg_file_new_size)
-                              / static_cast<double>(dbg_file_orig_size);
+            PLOGE_IF(dbg_file_ratio > 9) << fmt::format("File {} was enlarged by {:.2f}%: {} -> {}",
+                                                        path_for_log,
+                                                        (dbg_file_ratio - 1.0) * 100.0,
+                                                        dbg_file_orig_size,
+                                                        dbg_file_new_size);
 
-        PLOGE_IF(dbg_file_ratio > 9) << fmt::format("File {} was enlarged by {:.2f}%: {} -> {}",
-                                                    path_for_log,
-                                                    (dbg_file_ratio - 1.0) * 100.0,
-                                                    dbg_file_orig_size,
-                                                    dbg_file_new_size);
+            if (dbg_file_ratio > 9)
+            {
+                PLOGE << "Wrote file to disk for debugging purposes: " << path_for_log << "to "
+                      << btu::fs::absolute(path.filename()).string();
+                btu::common::write_file(path.filename(), ret.value());
+                btu::common::write_file(path.filename().replace_extension(".orig" + path.extension().string()),
+                                        dbg_file_orig_content);
+            }
 
-        if (dbg_file_ratio > 9)
-        {
-            PLOGE << "Wrote file to disk for debugging purposes: " << path_for_log << "to "
-                  << btu::fs::absolute(path.filename()).string();
-            btu::common::write_file(path.filename(), ret.value());
-            btu::common::write_file(path.filename().replace_extension(".orig" + path.extension().string()),
-                                    dbg_file_orig_content);
-        }
-
-        return std::move(*ret);
-    });
+            return std::move(*ret);
+        },
+        [](const btu::Path &archive_path, btu::modmanager::ModFolder::ArchiveTooLargeState state) {
+            switch (state)
+            {
+                case btu::modmanager::ModFolder::ArchiveTooLargeState::BeforeProcessing:
+                {
+                    PLOG_ERROR << fmt::format("Found archive {} that is too large", archive_path.string());
+                    rename_bad_file(archive_path);
+                    break;
+                }
+                case btu::modmanager::ModFolder::ArchiveTooLargeState::AfterProcessing:
+                {
+                    PLOG_ERROR << fmt::format("Archive {} became too large after processing. Ignoring it.",
+                                              archive_path.string());
+                    break;
+                }
+            }
+            return btu::modmanager::ModFolder::ArchiveTooLargeAction::Skip;
+        });
 
     const auto end_time     = QDateTime::currentDateTime();
     const auto elapsed_time = start_time.secsTo(end_time);
