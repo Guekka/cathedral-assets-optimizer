7a91086ec1070e2f107dc58c33d3e78028ca6cc3
G'k <39066502+Guekka@users.noreply.github.com>
2024-01-04 12:21:23 +0100

    build(deps): update mpsc-channel

--- cmake/ports/mpsc-channel/portfile.cmake
+++ cmake/ports/mpsc-channel/portfile.cmake
@@ -1,25 +1,26 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
-        OUT_SOURCE_PATH
-        SOURCE_PATH
-        REPO
-        Guekka/mpsc-channel-in-cxx
-        REF
-        f71892e19ca647dd2cb4fbc24617a8945092a2d8
-        SHA512
-        b1537446cc16f69131ff3218e9210067540a5b5ace029fe4d362c7a15a7d8d91dd73b3eef9231296838b26e7151932dacbfc951db2c77a0d67dc2b7c0538d239
-        HEAD_REF
-        main)
+  OUT_SOURCE_PATH
+  SOURCE_PATH
+  REPO
+  Guekka/mpsc-channel-in-cxx
+  REF
+  cdd4569b58b0518cefce10a99c5a47d7cae78891
+  SHA512
+  d6f9b0b539066b74eb0c43d391c99a2892c42780dba8a0ae5a28ad20bfd016329d0be237398a1db51d8b839f56152afbf3adea9173ded5a742a36a94001dc5d6
+  HEAD_REF
+  main)
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" OPTIONS -DBUILD_TESTING=OFF
-        ${FEATURE_OPTIONS})
+                      ${FEATURE_OPTIONS})
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME mpsc_channel CONFIG_PATH "lib/cmake/mpsc_channel")
+vcpkg_cmake_config_fixup(PACKAGE_NAME mpsc_channel CONFIG_PATH
+                         "lib/cmake/mpsc_channel")
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include
-        ${CURRENT_PACKAGES_DIR}/debug/share)
+     ${CURRENT_PACKAGES_DIR}/debug/share)
 
 file(
-        INSTALL "${SOURCE_PATH}/LICENSE"
-        DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-        RENAME copyright)
+  INSTALL "${SOURCE_PATH}/LICENSE"
+  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+  RENAME copyright)
