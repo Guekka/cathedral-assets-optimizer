6d2455ff2ec2dcf8938a83bdf159e5a93a405459
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-01-04 14:27:57 +0100

    build(nix): use official vcpkg port
    Would have been easier if it had not been stripped of the features I
    made...

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    vcpkg-repo.url = "github:Guekka/nixpkgs/vcpkg";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
   };
@@ -9,7 +8,6 @@
   outputs = {
     self,
     nixpkgs,
-    vcpkg-repo,
     devenv,
     systems,
     ...
@@ -20,33 +18,68 @@
       forEachSystem
       (system: let
         pkgs = nixpkgs.legacyPackages.${system};
-        vcpkg = vcpkg-repo.legacyPackages.${system}.vcpkg;
+        # TODO: upstream?
+        vcpkgDeps = with pkgs; [
+          pkg-config
+          autoconf
+          automake
+          zip
+          libtool
+        ];
+
+        nativePackages = [
+          "qtbase"
+          "qttools"
+        ];
       in {
         default = pkgs.mkShell {
-              nativeBuildInputs = with pkgs; [
-                cmake
-                makeWrapper
-                ninja
-                gcc
-                vcpkg
+          nativeBuildInputs = with pkgs;
+            [
+              cmake
+              makeWrapper
+              ninja
+              gcc
+              vcpkg
 
-                # profiling
-                linuxPackages_latest.perf
+              # profiling
+              linuxPackages_latest.perf
 
-                qt6.qtbase
-                qt6.qttools
-                qt6.wrapQtAppsHook
-              ];
+              qt6.qtbase
+              qt6.qttools
+              qt6.qtwayland
+              qt6.wrapQtAppsHook
+            ]
+            ++ vcpkgDeps;
 
-              shellHook = ''
-                export VCPKG_ROOT="$HOME/.local/share/vcpkg/root"
+          shellHook = ''
+            export VCPKG_ROOT=$(realpath $(dirname $(readlink -f $(type -p vcpkg)))/../share/vcpkg)
 
-                # set required environment variables for Qt
-                setQtEnvironment=$(mktemp --suffix .setQtEnvironment.sh)
-                makeShellWrapper "$(type -p sh)" "$setQtEnvironment" "''${qtWrapperArgs[@]}"
-                sed "/^exec/d" -i "$setQtEnvironment"
-                source "$setQtEnvironment"
-              '';
+            # set required environment variables for Qt
+            setQtEnvironment=$(mktemp --suffix .setQtEnvironment.sh)
+            makeShellWrapper "$(type -p sh)" "$setQtEnvironment" "''${qtWrapperArgs[@]}"
+            sed "/^exec/d" -i "$setQtEnvironment"
+            source "$setQtEnvironment"
+
+            # TODO: upstream?
+            # instead of fixing all the ports manually, we prompt the user to install them using Nix
+            # in the future, a better solution would be to generate a nix derivation from
+            # the vcpkg manifest
+
+            newPortsDir=$HOME/.vcpkg/overlay-ports
+            for port in ${pkgs.lib.concatStringsSep " " nativePackages}
+            do
+              portdir=$newPortsDir/$port
+              mkdir -p $portdir
+              echo 'set(VCPKG_POLICY_EMPTY_PACKAGE enabled)' > $portdir/portfile.cmake
+              echo "message(WARNING Please install $port with Nix)" >> $portdir/portfile.cmake
+              # We're going to manipulate the vcpkg.json here
+              # We want to preserve the structure, but remove all dependencies
+              ${pkgs.jq}/bin/jq 'walk(if type == "object" and has("dependencies") then .dependencies = [] else . end)' \
+                "$VCPKG_ROOT/ports/$port/vcpkg.json" > "$newPortsDir/$port/vcpkg.json"
+            done
+
+            export VCPKG_OVERLAY_PORTS=$newPortsDir:$VCPKG_OVERLAY_PORTS
+          '';
         };
       });
   };
