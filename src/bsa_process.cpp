2ccfdcf12d0871fbceef0695e0c1344bcd795063
Mikulas Klokocka <mklokocka@gmail.com>
2024-07-17 02:24:13 +0200

    feat: add an option to create override files #7

--- src/bsa_process.cpp
+++ src/bsa_process.cpp
@@ -5,6 +5,8 @@
 #include <btu/common/filesystem.hpp>
 #include <plog/Log.h>
 
+#include <fstream>
+
 /**
  * @brief Remove relative paths from a directory
  * Also removes directories made empty by the removal of the files
@@ -34,27 +36,29 @@ auto remove_files_from_directory(const btu::Path &directory, std::span<const btu
 void write_single_archive(const btu::Path &directory_path,
                           btu::bsa::Archive &&archive,
                           const btu::bsa::Settings &bsa_sets,
-                          const bool remove_files)
+                          const bool remove_files,
+                          const bool make_override)
 {
-    const auto archive_path = find_archive_name(directory_path, bsa_sets, archive.type());
-    if (!archive_path)
+    const auto archive_path_opt = find_archive_name(directory_path, bsa_sets, archive.type());
+    if (!archive_path_opt)
     {
         PLOGE << "Failed to find a name for the archive, skipping";
         return;
     }
+    auto archive_path = std::move(archive_path_opt).value();
 
-    if (const bool success = std::move(archive).write(archive_path.value()); !success)
+    if (const bool success = std::move(archive).write(archive_path); !success)
     {
-        PLOGE << "Failed to write archive " << archive_path.value().string();
+        PLOGE << "Failed to write archive " << archive_path.string();
         return;
     }
 
     // we open the archive again to make sure the files are written
-    auto written_archive = btu::bsa::Archive::read(archive_path.value());
+    auto written_archive = btu::bsa::Archive::read(archive_path);
     if (!written_archive)
     {
         PLOGE << "Archive was written but cannot be opened, it is likely corrupted. Removing it.";
-        btu::fs::remove(archive_path.value());
+        btu::fs::remove(archive_path);
     }
 
     if (remove_files)
@@ -67,5 +71,17 @@ void write_single_archive(const btu::Path &directory_path,
         written_archive.reset(); // close the archive before removing the files
         const auto count = remove_files_from_directory(directory_path, relative_paths);
         PLOGI << std::format("Attempted to remove {} files, {} were removed", relative_paths.size(), count);
+    }
+
+    if (make_override)
+    {
+        const auto override_path = std::move(archive_path).replace_extension(".override");
+
+        btu::common::write_file_new(override_path, {}).map_error([](const btu::common::Error &e) {
+            if (e == std::errc::file_exists)
+                return; // this is fine
+
+            PLOGE << "Failed to create override file: " << e;
+        });
     }
 }
