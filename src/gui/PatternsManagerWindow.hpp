2eb1f9bd8650901425e64ad50cb61eb107da6a5a
Mikulas Klokocka <mklokocka@gmail.com>
2024-08-23 05:00:35 +0200

    feat: patterns UX improvements #10

--- src/gui/PatternsManagerWindow.hpp
+++ src/gui/PatternsManagerWindow.hpp
@@ -5,7 +5,13 @@
 
 #pragma once
 
+#include "settings/profile.hpp"
+#include "settings/settings.hpp"
+
+#include <QAbstractItemModel>
 #include <QDialog>
+#include <QListWidget>
+#include <QListWidgetItem>
 class QComboBox;
 
 namespace Ui {
@@ -14,6 +20,18 @@ class PatternsManagerWindow;
 
 namespace cao {
 class Settings;
+
+class PatternItem : public QListWidgetItem
+{
+public:
+    explicit PatternItem(PerFileSettings &pfs, QListWidget *parent = nullptr);
+
+    const std::u8string update_text();
+
+private:
+    PerFileSettings &pfs_; // NOLINT(cppcoreguidelines-avoid-const-or-ref-data-members)
+};
+
 class PatternsManagerWindow final : public QDialog
 {
     Q_OBJECT
@@ -29,14 +47,15 @@ public:
 
     ~PatternsManagerWindow() override; // = default
 
-    void update_patterns(QComboBox &box);
+    void update_patterns(QListWidget &list);
 
 private:
     std::unique_ptr<Ui::PatternsManagerWindow> ui_;
     Settings &settings_; // NOLINT(cppcoreguidelines-avoid-const-or-ref-data-members)
 
+    void update_pattern(QListWidgetItem *item);
+    void move_pattern(const QModelIndex, int source_row, int, const QModelIndex, int destination_row);
     void create_pattern();
-
     void delete_current_pattern();
 };
 } // namespace cao
